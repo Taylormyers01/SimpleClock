b8e978adebf4b6ef061405410412c40e9731e6a9
taylor <taylormyers01@comcast.net>
2023-07-13 13:05:17 -0400

    Added first button and updted run to use threads

--- SimpleClock.java
+++ SimpleClock.java
@@ -2,20 +2,25 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimerTask;
 
 
-public class SimpleClock extends JFrame {
+public class SimpleClock extends JFrame implements Runnable {
     
         Calendar calendar;
         SimpleDateFormat timeFormat;
         SimpleDateFormat dayFormat;
         SimpleDateFormat dateFormat;
-    
+        Integer hours = 12;
         JLabel timeLabel;
         JLabel dayLabel;
         JLabel dateLabel;
+        JButton format;
+        JButton localGMT;
         String time;
         String day;
         String date;
@@ -24,30 +29,53 @@ public class SimpleClock extends JFrame {
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setTitle("Digital Clock");
             this.setLayout(new FlowLayout());
-            this.setSize(350, 220);
+            this.setSize(438, 320); //350 220
             this.setResizable(false);
     
             timeFormat = new SimpleDateFormat("hh:mm:ss a");
             dayFormat=new SimpleDateFormat("EEEE");
             dateFormat=new SimpleDateFormat("dd MMMMM, yyyy");
+
             timeLabel = new JLabel();
             timeLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 59));
             timeLabel.setBackground(Color.BLACK);
             timeLabel.setForeground(Color.WHITE);
             timeLabel.setOpaque(true);
+
             dayLabel=new JLabel();
             dayLabel.setFont(new Font("Ink Free",Font.BOLD,34));
     
             dateLabel=new JLabel();
             dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
+
+            format = new JButton();
+            format.setFont(new Font("Ink Free",Font.BOLD,24));
+            format.setText("Swap 12/24");
+            format.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(hours == 12){
+                        hours = 24;
+                        timeFormat.applyPattern("kk:mm:ss a");
+                    }
+                    else{
+                        hours = 12;
+                        timeFormat.applyPattern("hh:mm:ss a");
+                    }
+                }
+            });
+
+            
     
     
             this.add(timeLabel);
             this.add(dayLabel);
             this.add(dateLabel);
+            this.add(format);
             this.setVisible(true);
     
-            setTimer();
+            //setTimer();
+            new Thread(this).start();
         }
     
         public void setTimer() {
@@ -71,4 +99,24 @@ public class SimpleClock extends JFrame {
         public static void main(String[] args) {
             new SimpleClock();
         }
+
+    @Override
+    public void run() {
+        time = timeFormat.format(Calendar.getInstance().getTime());
+        timeLabel.setText(time);
+
+        day = dayFormat.format(Calendar.getInstance().getTime());
+        dayLabel.setText(day);
+
+        date = dateFormat.format(Calendar.getInstance().getTime());
+        dateLabel.setText(date);
+
+        try {
+            Thread.sleep(1000);
+            run();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
+
+}
